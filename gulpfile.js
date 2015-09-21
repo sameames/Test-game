4c50162d928123556343d499284d977c3f9f50ea
sameames <sross.eames@gmail.com>
2015-09-21 19:56:51 +1000

    Added gulp and scss to project. Configured gulp to create dist build.

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    del = require('del');
+
+gulp.task("concatScripts", function(){
+    return gulp.src([
+      'js/getElements.js',
+      'js/game.js',
+      'js/score.js',
+      'js/triggers.js'])
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest("js"));
+})
+
+gulp.task("minifyScripts", ['concatScripts'], function(){
+    return gulp.src("js/app.js")
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest("js"));
+})
+
+gulp.task('clean', function(){
+  del(['dist', 'js/app.*.js']);
+})
+
+gulp.task("build", ['minifyScripts'], function(){
+    return gulp.src(["css/main.css", "js/app.min.js", "index.html"], {base: './'})
+              .pipe(gulp.dest('dist'));
+});
+
+gulp.task("default", ['clean'], function (){
+  gulp.start("build");
+});
